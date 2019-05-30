636b87c9cbf8ebcf78af3ccc66c8d174c8fa50c1
songsang1998 <44784597+songsang1998@users.noreply.github.com>
2019-05-31 02:03:45 +0900

    Signed-off-by: songsang1998 <44784597+songsang1998@users.noreply.github.com>

---
+++ Assets/02.Scripts/Open.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour
+{
+    Animator anim;
+    bool s = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.K) && s==false)
+        {
+            anim.SetTrigger("Open");
+            s = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.K) && s == true)
+        {
+            anim.SetTrigger("Close");
+            s = false;
+        }
+    }
+}
