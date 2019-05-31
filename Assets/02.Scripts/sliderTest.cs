19a324c77cdd6ab25b45356d56b9f7474a6f7851
SEOHEECHO <50196951+SEOHEECHO@users.noreply.github.com>
2019-05-31 23:17:33 +0900

    Signed-off-by: SEOHEECHO <50196951+SEOHEECHO@users.noreply.github.com>

---
+++ Assets/02.Scripts/sliderTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; //UGUI에 접근하려면 추가
+public class sliderTest : MonoBehaviour
+{
+
+    public Slider progress;
+    //Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        progress.value = Mathf.MoveTowards(progress.value, 1.0of, 0.01f);
+
+    }
+}
