4bf4c527e351c6671a045a7bd87ebc2c7824f26a
songsang1998 <44784597+songsang1998@users.noreply.github.com>
2019-06-04 07:32:43 +0900

    Signed-off-by: songsang1998 <44784597+songsang1998@users.noreply.github.com>

---
+++ Assets/02.Scripts/Moster/Damages.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damages : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    Monster my;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        my = GetComponent<Monster>();
+    }
+    void MobDamage(int mDamage)
+    {
+        if (my.state != Monster.MonsterState.Die)
+        {
+
+            my.Hp -= mDamage;
+            StartCoroutine("Hit");
+            my.state = Monster.MonsterState.Damage;
+        }
+    }
+    IEnumerator Hit()
+    {
+
+
+        spriteRenderer.color = new Color32(0, 255, 255, 255);
+        yield return new WaitForSeconds(0.8f);
+        spriteRenderer.color = new Color(255, 255, 255, 255);
+        my.state = Monster.MonsterState.Tracks;
+
+    }
+}
--- Assets/02.Scripts/Moster/Monster.cs
+++ Assets/02.Scripts/Moster/Monster.cs
@@ -11,14 +11,14 @@ public class Monster : Character
 
    public static Transform target;
     public MonsterState state;
-    SpriteRenderer spriteRenderer;
+    
 
     // Start is called before the first frame update
     void Awake()
     {
 
         target =  GameObject.FindWithTag("Player").GetComponent<Transform>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        
 
     }
    
@@ -36,40 +36,15 @@ public class Monster : Character
             
 
         }
-        if (other.transform.name == "KPos")
-        {
-            state = MonsterState.Damage;
-            Hp = -10;
-            StartCoroutine("Hit");
-        }
 
     }
-    IEnumerator Hit()
-    {
-        int count;
-        Hp = -10;
-        for (count = 0; count < 10; count++)
-        {
-
-            if (count % 2 == 0)
-                spriteRenderer.color = new Color32(255, 255, 255, 90);
-            else
-                spriteRenderer.color = new Color32(255, 255, 255, 180);
-
-            yield return new WaitForSeconds(0.1f);
-
-            count++;
-        }
-        spriteRenderer.color = new Color(255, 255, 255, 255);
-        state = MonsterState.Tracks;
-
-    }
+  
 
         public void OnCollisionStay2D (Collision2D collision)
     {
-        if (collision.transform.tag == "Player")
+        if (collision.transform.name == "player")
         {
-            target.SendMessage("SetDamage", Damage);
+            collision.gameObject.SendMessage("SetDamage", Damage);
         }
        
     }
