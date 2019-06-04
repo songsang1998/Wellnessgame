d09a9d11d2b6f677aa1ba8b1b6c87f579b4879a1
songsang1998 <44784597+songsang1998@users.noreply.github.com>
2019-06-04 18:50:53 +0900

    Signed-off-by: songsang1998 <44784597+songsang1998@users.noreply.github.com>

---
+++ Assets/02.Scripts/ClickTwo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTwo : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int ID;
+    public Texture2D cursorTexture;
+    private Vector2 hotSpot;
+    GameObject s;
+    Texts pass1;
+    private void Start()
+    {
+
+        hotSpot.x = cursorTexture.width / 2;
+        hotSpot.y = cursorTexture.height / 2;
+        s = GameObject.Find("Canvas/UI_object_research/2option");
+        pass1 = GameObject.Find("Text").GetComponent<Texts>();
+    }
+
+
+    void OnMouseDown()
+    {
+        s.SetActive(true);
+       
+    }
+
+
+
+
+    public CursorMode cursorMode = CursorMode.Auto;
+
+    void OnMouseEnter()
+    {
+
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+
+
+
+}
